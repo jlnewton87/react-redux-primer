c5f01dba67a0c082646e6dd5d6d99056714bf3a3
Josh Newton <josh.newton@careerbuilder.com>
2018-02-09 11:19:41 -0500

    setup store and basic reducer (does nothing)

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+const initialState = { counter: 0 };
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
+
+export default reducer;
